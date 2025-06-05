30730a7814eca2e25dae99e1482acb8f440c1648
Dhruv Sharma <d.sharma200611@gmail.com>
2025-06-05 16:30:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,7 +3,7 @@ public:
     double myPow(double x, int n) {
         long long nn=n;
         double ans=1;
-        if (nn<0) nn=-1*nn;
+        if (n<0) nn=-1*nn;
         while (nn) {
             if (nn%2==1) {
                 ans=ans*x;
