c8aa37d52176dd3ca41ba68f95f86d847a6cc42d
Dhruv Sharma <d.sharma200611@gmail.com>
2025-06-06 19:30:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // Create a memoization array initialized to -1
+        // Create an array initialized to -1
         // It will store the number of ways to reach each step
         vector<int> memo(n + 1, -1);
         
-        // Call the helper recursive function with memoization
+        // Call the recursive function with array
         return climb(n, memo);
     }
 
-    // Helper function that uses recursion + memoization
+    // this is the helper recursive fucntion
     int climb(int n, vector<int>& memo) {
         // Base case: If n is 0 or 1, there is only 1 way to climb
         if (n == 0 || n == 1) return 1;
 
-        // If we have already calculated the result for this n, return it
+        // If we have already calculated the result for this n, then we will return it
         if (memo[n] != -1) return memo[n];
 
         // Otherwise, calculate the result and store it in memo
