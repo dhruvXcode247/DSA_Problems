98c6541194d0a5e8e2e2eac1d98e9639191cea78
Dhruv Sharma <d.sharma200611@gmail.com>
2025-09-11 00:14:16 +0530

    Time: 0 ms (100%), Space: 9.8 MB (80.8%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> subset;   // To store all subsets
-    
-    void backtrack(vector<int>& nums, int index, vector<int>& current) {
-        // Base case: If we reached the end of nums, add the current subset to the result
-        if (index == nums.size()) {
-            subset.push_back(current);
-            return;
-        }
-        
-        // Exclude nums[index]
-        backtrack(nums, index + 1, current);
-        
-        // Include nums[index]
-        current.push_back(nums[index]);
-        backtrack(nums, index + 1, current);
-        
-        // Backtrack: Remove the last element before returning to previous call
-        current.pop_back();
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> current;  // Temporary vector to store the current subset
-        backtrack(nums, 0, current);
-        return subset;
+        int n=nums.size();
+        vector<vector<int>>finalAns;
+        for (int i=0;i<(1<<n);i++) {
+            vector<int>ans={};
+            for (int j=0;j<n;j++) {
+                if (i&(1<<j)) {
+                    ans.emplace_back(nums[j]);
+                }
+            }
+            finalAns.emplace_back(ans);
+        }
+        return finalAns;
     }
 };
