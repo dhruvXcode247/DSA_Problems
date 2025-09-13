544c7fd4467c6fc74d587068d5b4b4842eec0ae3
Dhruv Sharma <d.sharma200611@gmail.com>
2025-09-13 19:25:49 +0530

    Time: 0 ms (100%), Space: 9.8 MB (57.27%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<char,int>mpp;
+        int vowcount=0,conscount=0;
+        for (int i=0;i<s.size();i++) {
+            mpp[s[i]]++;
+        }
+        int maxi=INT_MIN;
+        for (auto it:mpp){
+            if (it.first=='a'||it.first=='e'||it.first=='i'||it.first=='o'|| it.first=='u'){
+                vowcount=max(vowcount,it.second);
+            }
+            else {
+                conscount=max(conscount,it.second);
+            }
+        }
+        return vowcount+conscount;
+    }
+};
