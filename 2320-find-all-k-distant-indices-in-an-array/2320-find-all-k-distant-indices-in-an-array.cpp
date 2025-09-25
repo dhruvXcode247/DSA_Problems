0192fab90eceb8206e9ca3c63043ee11b2072c3e
Dhruv Sharma <d.sharma200611@gmail.com>
2025-09-25 19:38:13 +0530

    Time: 15 ms (29.71%), Space: 15.9 MB (22.41%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -5,7 +5,9 @@ public:
         unordered_set<int>st;
         for (int i=0;i<n;i++) {
             for (int j=0;j<n;j++) {
-                if (abs(i-j)<=k && nums[j]==key) st.insert(i);
+                if (abs(i-j)<=k && nums[j]==key) {
+                    st.insert(i); break;
+                }
             }
         }
         vector<int>ans(st.begin(),st.end());
