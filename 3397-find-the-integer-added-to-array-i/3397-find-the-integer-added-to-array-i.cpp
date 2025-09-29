5191796e947881572715f62f9d2122064554dc84
Dhruv Sharma <d.sharma200611@gmail.com>
2025-09-29 21:37:38 +0530

    Time: 0 ms (100%), Space: 35.3 MB (24.7%) - LeetHub

--- 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.cpp
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        return nums2[0]-nums1[0];
+        long long sum1=0,sum2=0;
+        for (int i:nums1) {
+            sum1+=i;
+        }
+        for (int i:nums2) {
+            sum2+=i;
+        }
+        return (int)((sum2-sum1)/(long long)(nums2.size()));
     }
 };
