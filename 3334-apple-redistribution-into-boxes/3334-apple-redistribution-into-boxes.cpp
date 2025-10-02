fac3634192d2e12ba61186414900a75d0a0cd2ca
Dhruv Sharma <d.sharma200611@gmail.com>
2025-10-03 00:26:07 +0530

    Time: 0 ms (100%), Space: 33.2 MB (51.18%) - LeetHub

---
+++ 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int count=0,sum=0,capSum=0;
+        sort(capacity.begin(),capacity.end(),greater<int>());
+        for (int i=0;i<apple.size();i++) {
+            sum+=apple[i];   
+        }
+        for (int i=0;i<capacity.size();i++) {
+            capSum+=capacity[i];
+            count++;
+            if (capSum>=sum) return count;
+        }
+        return count;
+    }
+};
