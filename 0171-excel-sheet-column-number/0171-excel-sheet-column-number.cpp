bdac883c04ad71a36b519e950b837db384da5e39
Dhruv Sharma <d.sharma200611@gmail.com>
2025-10-04 16:19:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.52%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        int ans=0,n=columnTitle.size();
+        int ans=columnTitle[0]-'A'+1,n=columnTitle.size();
         if (columnTitle.size()==1) return columnTitle[0]-'A'+1;
-        for (int i=0;i<n;i++) {
+        for (int i=1;i<n;i++) {
             ans=(26*ans+(columnTitle[i]-'A')+1);
         }
         return ans;
