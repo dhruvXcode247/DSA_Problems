aa0f18cf1da0524b22cf2aad714cc80e7dd5191a
Dhruv Sharma <d.sharma200611@gmail.com>
2025-11-11 00:12:25 +0530

    Time: 24 ms (9.79%), Space: 11 MB (23.69%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int josephus (vector<bool>person,int n,int index,int person_left,int k) {
+        if (person_left==1) {
+            for (int i=0;i<n;i++) {
+                if (person[i]==0) return i+1;
+            }
+        }
+        int kill=(k-1)%person_left;
+        while (kill--) {
+            index=(index+1)%n;
+            while (person[index]==1) {
+                index=(index+1)%n;
+            }
+        }
+        person[index]=1;
+        while (person[index]==1) {
+            index=(index+1)%n;
+        }
+        return josephus(person,n,index,person_left-1,k);
+    }
+    int findTheWinner(int n, int k) {
+        vector<bool>visited(n,0);
+        return josephus(visited,n,0,n,k);
+    }
+};
