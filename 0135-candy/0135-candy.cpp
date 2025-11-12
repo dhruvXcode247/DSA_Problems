c69bdad83294b28826e9a47e420193ed54cb08e1
Dhruv Sharma <d.sharma200611@gmail.com>
2025-11-12 23:43:03 +0530

    Time: 0 ms (100%), Space: 22.1 MB (84.26%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n=ratings.size(),candies=0;
-        vector<int>left(n,1),right(n,1);
-        for (int i=1;i<n;i++) {
-            if (ratings[i]>ratings[i-1]) {
-                left[i]=left[i-1]+1;
+        int i=1,n=ratings.size(),candies=1;
+        while (i<n) {
+            if (ratings[i]==ratings[i-1]) {
+                candies++,i++;
+                continue;
             }
-            else left[i]=1;
-        }
-        for (int i=n-2;i>=0;i--) {
-            if (ratings[i]>ratings[i+1]) {
-                right[i]=right[i+1]+1;
+            int peak=1;
+            while (i<n && ratings[i]>ratings[i-1]) {
+                peak++,candies+=peak,i++;
             }
-            else right[i]=1;
-        }
-        for (int i=0;i<n;i++) {
-            candies+=max(left[i],right[i]);
+            int down=1;
+            while (i<n && ratings[i]<ratings[i-1]) {
+                candies+=down,i++,down++;
+            }
+            if (down>peak) candies+=(down-peak);
         }
         return candies;
     }
