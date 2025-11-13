7299d1cbb6272cc0ebf7c752e252614b7641e5ec
Dhruv Sharma <d.sharma200611@gmail.com>
2025-11-14 00:41:32 +0530

    Time: 167 ms (93.98%), Space: 110.3 MB (95.25%) - LeetHub

--- 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -4,13 +4,12 @@ public:
         unordered_set<int>st(nums.begin(),nums.end());
         int count=0,n=nums.size();
         for (int i=0;i<n;i++) {
-            if (nums[i]>=0 && nums[i]<=9) continue;
             int num=nums[i],revnum=0;
             while (num>0) {
                 revnum=(revnum*10)+num%10;
                 num/=10;
             }
-            if (revnum!=nums[i]) st.insert(revnum);
+            st.insert(revnum);
         }
         return st.size();
     }
