5113da95bf7dbd074d6cbf7d076a3c8fb161be28
Dhruv Sharma <d.sharma200611@gmail.com>
2025-11-17 19:23:28 +0530

    Time: 1 ms (22.91%), Space: 61.3 MB (97.09%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>vec;
+        int n=nums.size(),idx=-1;
         for (int i=0;i<n;i++) {
-            if (nums[i]==1) {
-                vec.emplace_back(i);
+            if (nums[i]==1 && idx==-1) {
+                idx=i;
             }
-        }
-        int n1=vec.size();
-        for (int i=0;i<n1-1;i++) {
-            if (vec[i+1]-vec[i]-1<k) return false;
+            else if (nums[i]==1 && idx!=-1) {
+                if (i-idx-1<k) return false;
+                idx=i;
+            }
         }
         return true;
     }
