bafa0b19b7cad6e0bed8fe27b68e7e6d872cad91
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-04 15:06:53 +0530

    Time: 24 ms (58.87%), Space: 16.1 MB (92.61%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-        int n=nums.size(),sum=0;
+        int n=nums.size(),sum=0,total=0;
         unordered_set<int>st;
         for (int i=0;i<n;i++) {
-            int num=nums[i],count=2;
+            int num=nums[i],count=2,sum=1+num;
             for (int j=2;j*j<=num;j++) {
                 if (num%j==0) {
-                    st.insert(j);
+                    sum+=j;
                     count++;
                     int other=num/j;
                     if (other!=j) {
-                        st.insert(other);
+                        sum+=other;
                         count++;
                     }
                 }
             }
             if (count==4) {
-                st.insert(num);
-                st.insert(1);
-                for (auto it:st) {
-                    sum+=it;
-                }
+                total+=sum;
             }
-            st.clear();
+            sum=0;
         }
-        return sum;
+        return total;
     }
 };
