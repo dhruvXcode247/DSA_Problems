08f864791881bfb7acf10c4292c386fcbb9265d3
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-07 13:51:35 +0530

    Time: 0 ms (100%), Space: 33 MB (96.03%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -4,7 +4,7 @@ public:
         int count=0,sum=0,capSum=0;
         sort(capacity.begin(),capacity.end(),greater<int>());
         for (int i=0;i<apple.size();i++) {
-            sum+=apple[i];   
+            sum+=apple[i];
         }
         for (int i=0;i<capacity.size();i++) {
             capSum+=capacity[i];
