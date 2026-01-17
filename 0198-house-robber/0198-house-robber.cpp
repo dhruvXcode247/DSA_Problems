407b7e2fcc665edfa1e068eec681eb70e2975717
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-17 20:25:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (61.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    // Helper function that uses recursion + memoization
-    int robFrom(vector<int>& nums, int i, vector<int>& memo) {
-        // Base case: if index goes out of bounds, return 0
-        if (i >= nums.size()) return 0;
-
-        // If we've already solved this subproblem, return the stored result
-        if (memo[i] != -1) return memo[i];
-
-        // Rob the current house and move to i+2
-        int rob = nums[i] + robFrom(nums, i + 2, memo);
-
-        // Skip the current house and move to i+1
-        int skip = robFrom(nums, i + 1, memo);
-
-        // Store the result in memo and return the max of the two options
-        return memo[i] = max(rob, skip);
+    int f(int ind,vector<int>&nums,vector<int>&dp) {
+        if (ind==0) return nums[ind];
+        if (ind<0) return 0;
+        if (dp[ind]!=-01) return dp[ind];
+        int pick=nums[ind]+f(ind-2,nums,dp);
+        int notPick=0+f(ind-1,nums,dp);
+        return dp[ind]=max(pick,notPick);
     }
-
     int rob(vector<int>& nums) {
-        // Create a memo array to store results of subproblems, initialized to -1
-        vector<int> memo(nums.size(), -1);
-
-        // Start recursion from the first house (index 0)
-        return robFrom(nums, 0, memo);
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return f(n-1,nums,dp);
     }
 };
