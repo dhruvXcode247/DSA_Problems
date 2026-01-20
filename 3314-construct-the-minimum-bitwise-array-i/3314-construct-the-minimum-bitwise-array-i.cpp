735878e73ef430a0fb72a3287d385a8d071a0e3e
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-20 11:11:59 +0530

    Time: 0 ms (100%), Space: 25.1 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int>ans(n,-1);
         for (int i=0;i<n;i++) {
-            for (int j=0;j<=nums[i];j++) {
+            for (int j=0;j<nums[i];j++) {
                 if ((j|(j+1)) == nums[i]) {
                     ans[i]=j;
                     break;
