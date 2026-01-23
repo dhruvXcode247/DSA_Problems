6f298c6bf4c6326133c0317e44f85c55d5e4df00
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-23 19:07:35 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.47%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    void subsequences(vector<int>&nums,vector<vector<int>>
-    &ans,vector<int>&temp,int start,int n) {
+    void subsequences(vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,int start,int n) {
         if (start==n) {
             ans.push_back(temp);
             return;
         }
-        subsequences(nums,ans,temp,start+1,n);
         temp.emplace_back(nums[start]);
         subsequences(nums,ans,temp,start+1,n);
         temp.pop_back();
+        subsequences(nums,ans,temp,start+1,n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size(),start=0;
