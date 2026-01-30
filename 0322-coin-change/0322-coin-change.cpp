7dbe4370b906c352c590811f96868ed9abce8306
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-31 02:14:45 +0530

    Time: 55 ms (18.75%), Space: 48.8 MB (38.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int fewest(vector<int>&coins,int amount,int ind,vector<vector<int>>&dp) {
-        if (amount==0) return 0;
-        if (ind == 0) {
-            if (amount%coins[ind]==0) {
-                return amount/coins[ind];
-            }
-            else return 1e9;
-        }
-        if (dp[ind][amount]!=-1) return dp[ind][amount];
-        int notTake=0+fewest(coins,amount,ind-1,dp);
-        int take=1e9;
-        if (coins[ind]<=amount) take=1+fewest(coins,amount-coins[ind],ind,dp);
-        return dp[ind][amount]=min(take,notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = fewest(coins, amount, n - 1, dp);
-        return (ans >= 1e9 ? -1 : ans);
-
+        for (int target=0;target<=amount;target++) {
+            if (target%coins[0]==0) {
+                dp[0][target]=target/coins[0];
+            }
+            else {
+                dp[0][target]=1e9;
+            }
+        }
+        for (int i=1;i<n;i++) {
+            for (int t=0;t<=amount;t++) {
+                int notTake=0+dp[i-1][t];
+                int take=1e9;
+                if (coins[i]<=t && dp[i][t - coins[i]] != 1e9) {
+                    take=1+dp[i][t-coins[i]];
+                }
+                dp[i][t]=min(take,notTake);
+            }
+        }
+        return (dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount]);
     }
 };
