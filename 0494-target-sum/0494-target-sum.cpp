5184c041dc3bf21f35148be9c3508d975405cc48
Dhruv Sharma <d.sharma200611@gmail.com>
2026-01-31 17:19:23 +0530

    Time: 170 ms (32.16%), Space: 11.7 MB (83.66%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int expressions(int n,vector<int>&nums,int target) {
+        if (n==0) {
+            if (nums[n]==0 && target==0) return 2;
+            if (target==0 || nums[0]==target) return 1;
+            return 0; 
+        }
+        int notTake=expressions(n-1,nums,target);
+        int take=0;
+        if (nums[n]<=target) take=expressions(n-1,nums,target-nums[n]);
+        return (take+notTake);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size(),totsum=0;
+        for (int i=0;i<n;i++) {
+            totsum+=nums[i];
+        }
+        if (totsum+target<0 || (totsum+target)%2) return 0;
+        return expressions(n-1,nums,(totsum-target)/2);
+    }
+};
