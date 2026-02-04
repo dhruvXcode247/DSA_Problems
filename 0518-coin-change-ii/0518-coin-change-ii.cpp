64b2306ae9a871e972271ab1eb30c64c67badc68
Dhruv Sharma <d.sharma200611@gmail.com>
2026-02-04 15:09:45 +0530

    Time: 27 ms (63.96%), Space: 36.7 MB (47.02%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int combinations (int idx, int amount, vector<int>&coins,vector<vector<int>>&dp) {
+        if (idx==0) {
+            return (amount%coins[0]==0);
+        }
+        if (dp[idx][amount]!=-1) return dp[idx][amount];
+        int notTake=combinations(idx-1,amount,coins,dp);
+        int take=0;
+        if (coins[idx]<=amount) take+=combinations(idx,amount-coins[idx],coins,dp);
+        return dp[idx][amount]=take+notTake;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return combinations(n-1,amount,coins,dp);
+    }
+};
