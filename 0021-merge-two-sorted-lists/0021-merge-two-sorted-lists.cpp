638150984fd016b58f2034db19399b273d9aad86
Dhruv Sharma <d.sharma200611@gmail.com>
2026-02-20 18:10:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (61.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -26,16 +26,8 @@ public:
             }
             temp=temp->next;
         }
-        while (list1!=nullptr) {
-            temp->next=list1;
-            temp=temp->next;
-            list1=list1->next;
-        }
-        while (list2!=nullptr) {
-            temp->next=list2;
-            temp=temp->next;
-            list2=list2->next;
-        }
+        if (list1) temp->next=list1;
+        if (list2) temp->next=list2;
         return dummy.next;
     }
 };
