85de604920ad2f9e83766ae65bfec08d7cbbc15b
Dhruv Sharma <d.sharma200611@gmail.com>
2026-02-21 22:45:39 +0530

    Time: 31 ms (14.84%), Space: 8 MB (35.12%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isprime(int n) {
         if (n<=1) return false;
-        if (n<=3) return true;
-        if (n%2==0 || n%3==0) return false;
-        for (int i=5;i<sqrt(n);i++) {
+        for (int i=2;i*i<=n;i++) {
             if (n%i==0) return false;
         }
         return true;
