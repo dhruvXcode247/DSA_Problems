8b2aa0acc0852b4b234d0bde9269327bf9a7f3de
Dhruv Sharma <d.sharma200611@gmail.com>
2026-02-24 19:12:34 +0530

    Time: 0 ms (100%), Space: 17.3 MB (53.22%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -11,29 +11,13 @@
  */
 class Solution {
 public:
-    void path(TreeNode* root,vector<int>&temp,vector<vector<int>>&ans) {
-        if (root==nullptr) return;
-        temp.emplace_back(root->val);
-        if (root->left==nullptr && root->right==nullptr) {
-            ans.emplace_back(temp);
-        }
-        if (root->left!=nullptr) path(root->left,temp,ans);
-        if (root->right!=nullptr) path(root->right,temp,ans);
-        temp.pop_back();
+    int path(TreeNode* root,int sum) {
+        if (root==nullptr) return 0;
+        sum=sum*2+root->val;
+        if (root->left==nullptr && root->right==nullptr) return sum;
+        return path(root->left,sum)+path(root->right,sum);
     }
     int sumRootToLeaf(TreeNode* root) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        path(root,temp,ans);
-        int sum=0;
-        int n=ans.size();
-        for (int i=0;i<n;i++) {
-            int num=0,size=ans[i].size();
-            for (int j=0;j<size;j++) {
-                num=num*2+ans[i][j];
-            }
-            sum+=num;
-        }
-        return sum;
+        return path(root,0);
     }
 };
