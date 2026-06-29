856f7e49b4283366215e26bc7e5c74ce0bcf46ea
Dhruv Sharma <d.sharma200611@gmail.com>
2026-06-29 16:47:34 +0530

    Time: 62 ms (80%), Space: 186.2 MB (80%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end(),greater<int>());
         k=min(k,int(n));
         for (int i=0;i<k;i++) {
-            if (mul>1) {
+            if (mul>=1) {
                 sum+=1LL*nums[i]*mul;
             }
             else {
