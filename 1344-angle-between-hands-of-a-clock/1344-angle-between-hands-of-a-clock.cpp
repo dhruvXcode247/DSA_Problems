41e733b50adfe99687a76b5cf57e6d943e9c1465
Dhruv Sharma <d.sharma200611@gmail.com>
2026-07-16 21:47:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,10 +2,5 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         
-        double minuteangle=minutes*6;
-        double hourangle=(hour%12)*30+minutes*0.5;
-        double answer=abs(hourangle-minuteangle);
-
-        return min(360-answer,answer);
     }
 };
